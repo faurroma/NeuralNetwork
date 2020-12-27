bbda5f87100b6718a30726ab0d79795d83812c34
Faure <romain.faure1@grenoble-inp.org>
2020-12-27 21:39:16 +0100

    MNIST

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,17 @@
 #include "Layer.hpp"
 #include "Model.hpp"
 #include "Functions.hpp"
+#include "mnist/mnist_reader.hpp"
 
+using namespace std;
 
+/* XOR
 
 int main(){
 	cout << "Test beginning" << endl;
 	Model testMod("binaryCrossEntropy", 0.1);
-	testMod.add(Layer(2, 4), "tanh");
-	testMod.add(Layer(4, 1), "sigmoid");
+	testMod.add(Layer(2, 3), "tanh");
+	testMod.add(Layer(3, 1), "sigmoid");
 
 	vector<double> i1 = {0, 0};
 	vector<double> i2 = {0, 1};
@@ -41,4 +44,17 @@ int main(){
 	return 0;
 
 }
+
+*/
+int main (){
+	// Load MNIST data
+	    mnist::MNIST_dataset<vector, vector<uint8_t>, uint8_t> dataset =
+	        mnist::read_dataset<vector, vector, uint8_t, uint8_t>("datasets");
+
+	    cout << "Nbr of training images = " << dataset.training_images.size() << endl;
+	    cout << "Nbr of training labels = " << dataset.training_labels.size() << endl;
+	    cout << "Nbr of test images = " << dataset.test_images.size() << endl;
+	    cout << "Nbr of test labels = " << dataset.test_labels.size() << endl;
+}
+
 
