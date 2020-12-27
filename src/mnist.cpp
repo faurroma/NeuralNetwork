9c6b00681db7c447f33db7d3f6ebe130cc5197b8
rouxst <stella.roux@grenoble-inp.org>
2020-12-27 21:07:04 +0100

    Ajout mnist

---
+++ src/mnist.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 21 déc. 2020
+ *      Author: faureromain
+ */
+#include <iostream>
+#include <string>
+#include "Layer.hpp"
+#include "Model.hpp"
+#include "Functions.hpp"
+
+
+
+int main(){
+
+}
